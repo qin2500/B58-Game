eecd55a0df782cdadc2cf374862f086af10af500
qin2500 <52422134+qin2500@users.noreply.github.com>
2023-07-19 22:56:02 -0400

    created project

---
+++ game.asm
@@ -0,0 +1,17 @@
+# Bitmap display starter code
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 4
+# - Unit height in pixels: 4
+# - Display width in pixels: 256
+# - Display height in pixels: 256
+# - Base Address for Display: 0x10008000 ($gp)
+#
+.eqv BASE_ADDRESS 0x10008000
+
+.globl main
+.text
+main: 
+
+
+upadte: 
